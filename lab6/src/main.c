b529a48fdd1113001c0ee2e7c9ca9b06a48d930c
Anton Yermilov <anton.yermilov@gmail.com>
2016-10-26 01:33:56 +0300

    lab6: init commit

---
+++ lab6/src/main.c
@@ -0,0 +1,35 @@
+#include "stdlib.h"
+#include "stddef.h"
+#include "assert.h"
+
+#include "phonebook.h"
+
+void phonebook_in(char* infile) {
+	phonebook_t book;
+	init_phonebook(&book);
+	assert(load_phonebook_xml(infile, &book) == 0);
+	print_phonebook(&book);
+	clear_phonebook(&book);
+}
+
+void phonebook_out(char* outfile, size_t MAX_SIZE) {
+	phonebook_t book;
+	init_phonebook(&book);
+	size_t size = rand() % MAX_SIZE + 1;
+	gen_phonebook(&book, size);
+	save_phonebook_xml(outfile, &book);
+	clear_phonebook(&book);
+}
+
+int main(int argc, char* argv[]) {
+	
+	assert(argc >= 3);
+	phonebook_in(argv[1]);
+	phonebook_out(argv[2], 50);
+
+	//phonebook_in("pbook.xml");
+	//phonebook_out("pbook_out.xml", 25);
+	//phonebook_in("pbook_out.xml");
+
+	return 0;
+}
